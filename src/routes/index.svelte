83761a23b49c7eb55ba69830a094f4279c148b1d
Filip Alexandrov <filipalexandrov@gmail.com>
2022-02-25 14:55:38 +0100

    todo delete/ done logic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,17 +8,35 @@
 		todos = [...todos, { task: newTodo, isComplete: false, createdAt: new Date() }];
 		newTodo = '';
 	};
+
+	const markTodoAsComplete = (index) => {
+		todos[index].isComplete = !todos[index].isComplete;
+		console.log(todos);
+	};
+
+	const deleteTodo = (index) => {
+		let deleteItem = todos[index];
+		todos = todos.filter((item) => item != deleteItem);
+	};
 </script>
 
 <input type="text" placeholder="Add a task" bind:value={newTodo} />
 <button on:click={addTodo}>Add</button>
 
 <ol>
-	{#each todos as todo}
+	{#each todos as todo, index}
 		<!-- if isComplete==true, class='complete' -->
-		<li class:complete={todo.isComplete}>
-			{todo.task}
+		<li>
+			<span class:complete={todo.isComplete}>
+				{todo.task}
+			</span>
+			<span>
+				<button on:click={() => markTodoAsComplete(index)}>✔</button>
+				<button on:click={() => deleteTodo(index)}>❌</button>
+			</span>
 		</li>
+	{:else}
+		<p>No Todos</p>
 	{/each}
 </ol>
 
