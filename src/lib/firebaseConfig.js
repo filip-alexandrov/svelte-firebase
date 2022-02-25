8c83fef4b0b1a04f075d198c6dcac43c821ec98f
filip-alexandrov <79532720+filip-alexandrov@users.noreply.github.com>
2022-02-25 15:59:56 +0100

    Update firebaseConfig.js

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,8 +1,2 @@
 export const firebaseConfig = {
-	apiKey: 'AIzaSyBeL4anM5OOi3NIxxFiAvChtfoGYbscixk',
-	authDomain: 'fir-svelte-8209a.firebaseapp.com',
-	projectId: 'fir-svelte-8209a',
-	storageBucket: 'fir-svelte-8209a.appspot.com',
-	messagingSenderId: '74638785312',
-	appId: '1:74638785312:web:a8987c47415b0d6a0d3545'
 };
